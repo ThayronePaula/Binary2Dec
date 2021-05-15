3e5a5f0e8b02586dbaa4842a8fc5d821702cb060
Thayrone <thayronedepaula15@gmail.com>
2021-05-15 19:28:14 -0300

    version 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./css/App.css";
+const App = () => {
+  const [bin, setBin] = React.useState("");
+  const [dec, setDec] = React.useState(null);
 
-function App() {
+  
+  function getNumber({target}) {
+    const regex = /^[0-1]+$/;
+    if (regex.test(bin)) {
+      let total = bin.length;
+      let soma = 0;
+      for (let i = 0; i < bin.length; i++) {
+        total--;
+        soma = soma + bin[total] * Math.pow(2, i);
+      }
+      target.value = soma
+      setDec(soma);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Binário para decimal</h1>
+      <p>entre com um binario e obtenha um decimal</p>
+      {dec && <p>{dec}</p>}
+      <input
+        type="text"
+        value={bin}
+        onChange={({ target }) => setBin(target.value)}
+      />
+      <button onClick={getNumber} >Converter</button>
+      
     </div>
   );
-}
+};
 
 export default App;
