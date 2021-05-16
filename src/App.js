5a8aa7004d98adcdc60e6daae8d37e903eb71443
Thayrone <thayronedepaula15@gmail.com>
2021-05-16 11:27:39 -0300

    final version

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import "./css/App.css";
 const App = () => {
   const [bin, setBin] = React.useState("");
   const [dec, setDec] = React.useState(null);
+  const [error, setError] = React.useState(null);
 
-  
-  function getNumber({target}) {
+  React.useEffect(() => {
     const regex = /^[0-1]+$/;
+
+    if (!bin || bin === "") {
+      setDec(null);
+      return;
+    }
+
+    if (!regex.test(bin)) {
+      setError("Esperando um número binário válido...");
+      setDec(null);
+    } else {
+      setError(null);
+    }
+
     if (regex.test(bin)) {
       let total = bin.length;
       let soma = 0;
@@ -14,23 +27,29 @@ const App = () => {
         total--;
         soma = soma + bin[total] * Math.pow(2, i);
       }
-      target.value = soma
       setDec(soma);
     }
-  }
+  }, [bin]);
 
   return (
     <div className="container">
-      <h1>Binário para decimal</h1>
-      <p>entre com um binario e obtenha um decimal</p>
-      {dec && <p>{dec}</p>}
-      <input
-        type="text"
-        value={bin}
-        onChange={({ target }) => setBin(target.value)}
-      />
-      <button onClick={getNumber} >Converter</button>
-      
+      <div className="content">
+        <h1>Binário para decimal</h1>
+        {bin && !error ? (
+          <p style={{ color: "#00ff00" }}>Aqui está seu número em decimal</p>
+        ) : (
+          <p>Digite um número Binário!</p>
+        )}
+        {bin && <h2>{dec}</h2>}
+        <input
+          minLength="1"
+          maxLength="20"
+          type="text"
+          value={bin}
+          onChange={({ target }) => setBin(target.value)}
+        />
+        {error && <p style={{ color: "red", fontWeight: "bold" }}>{error}</p>}
+      </div>
     </div>
   );
 };
